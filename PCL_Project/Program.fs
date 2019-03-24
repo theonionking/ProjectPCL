a3cb37dc657a4496a231fcfbddbfed6b1f081197
Antonio-Marius Tricolici <253966@VIAUC.DK>
2019-03-24 14:59:54 +0100

    Les go

--- PCL_Project/Program.fs
+++ PCL_Project/Program.fs
@@ -1,5 +1,25 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿
+// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
+
+let calc_price food_type size = 
+    match food_type with 
+    |"salad" -> 30 * sizeRP size
+    |"sandwich" -> 25* sizeRP size
+    |"cake" -> 20 * sizeRP size
+    |_ -> 0
+
+
+
+
+let sizeRP size= 
+    match size with
+    |"small" -> 0.5
+    |"medium" -> 1.0
+    |"large" -> 1.5
+    |_ -> 1.0
+
+
 
 [<EntryPoint>]
 let main argv = 
